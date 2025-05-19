1d0ef22790272b4e870053597d1439dfb3fdce30
Ian McGillivary <wiian16@icloud.com>
2025-05-18 20:48:49 -0500

    Updated Lazy lsp setup to use built-in lsp

--- Lazy/lua/wiian/plugins/lsp.lua
+++ Lazy/lua/wiian/plugins/lsp.lua
@@ -1,134 +1,168 @@
 return {
-    -- Lazy.nvim setup for mason and lsp-zero
+    ------------------------------------------------------------------------------
+    -- Mason: install/manage LSP servers, linters, formatters, etc.
+    ------------------------------------------------------------------------------
     {
-        "williamboman/mason.nvim",
-        build = ":MasonUpdate", -- Run MasonUpdate after installation
+        "mason-org/mason.nvim",
+        build = ":MasonUpdate",
         config = function()
             require("mason").setup()
+
+            -- Enable all diagnostic displays
+            vim.diagnostic.config({
+                virtual_text = {
+                    prefix = "■", -- Could be '●', '■', '▎', etc.
+                    spacing = 2, -- How much space between text and virtual text
+                },
+                signs = true, -- Show icons in the sign column
+                underline = true, -- Underline problematic text
+                update_in_insert = false, -- Don’t update diagnostics while in insert mode
+                severity_sort = true, -- Sort diagnostics by severity
+            })
         end,
     },
+
+    ------------------------------------------------------------------------------
+    -- Mason-LSPConfig v2: bridge Mason & nvim-lspconfig, now with `handlers` in setup
+    ------------------------------------------------------------------------------
     {
-        "williamboman/mason-lspconfig.nvim",
+        "mason-org/mason-lspconfig.nvim",
         dependencies = {
-            "williamboman/mason.nvim",
+            "mason-org/mason.nvim",
             "neovim/nvim-lspconfig",
         },
         config = function()
-            require("mason-lspconfig").setup {
-                ensure_installed = {
-                    "lua_ls",  -- Lua LSP
-                    "pyright", -- Python LSP
-                    "ts_ls",   -- TypeScript/JavaScript LSP
-                    "clangd",  -- C/C++ LSP
-                },
-                automatic_installation = true,
-            }
+            local mlsp         = require("mason-lspconfig")
+            local lspconfig    = require("lspconfig")
+            local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-            require("mason-lspconfig").setup_handlers {
-                -- Default server handler, called on installed servers that don't already have a handler
-                function(server_name)
-                    require("lspconfig")[server_name].setup {}
-                end,
-                -- Specific language server setup_handlers
-                ["lua_ls"] = function()
-                    require("lspconfig").lua_ls.setup {
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = { "vim" },
-                                },
-                            },
-                        },
-                    }
-                end,
-                -- Marksman LSP settings (disabling signatureHelp)
-                ["marksman"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.marksman.setup {
-                        handlers = {
-                            ["textDocument/signatureHelp"] = function() end, -- Disables signatureHelp
-                        },
-                    }
-                end,
-            }
-        end,
-    },
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v2.x",
-        dependencies = {
-            -- LSP Support
-            { "neovim/nvim-lspconfig" },
-            { "williamboman/mason.nvim" },
-            { "williamboman/mason-lspconfig.nvim" },
-
-            -- Autocompletion
-            { "hrsh7th/nvim-cmp" },
-            { "hrsh7th/cmp-nvim-lsp" },
-            { "hrsh7th/cmp-buffer" },
-            { "hrsh7th/cmp-path" },
-            { "hrsh7th/cmp-nvim-lsp-signature-help" },
-            { "saadparwaiz1/cmp_luasnip" },
-            { "L3MON4D3/LuaSnip" }, -- Snippets plugin
-        },
-        config = function()
-            local lsp = require("lsp-zero").preset("recommended")
-            -- lsp.configure("ts_ls", {
-            --     init_options = {
-            --         preferences = {
-            --             importModuleSpecifierPreference = "relative", -- or "non-relative"
-            --             importModuleSpecifierEnding = "auto",
-            --         }
-            --     }
-            -- })
-
-            -- Setup LSP mappings and keybinds
-            lsp.on_attach(function(client, bufnr)
-                local opts = { noremap = true, silent = true }
-                local keymap = vim.api.nvim_buf_set_keymap
-
-                -- Use `bufnr` directly as the buffer number
-                keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-                keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-                keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-                keymap(bufnr, "n", "go", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-                keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-                keymap(bufnr, "n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-                keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-                keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-                keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-                keymap(bufnr, "n", "<F3>", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
-                keymap(bufnr, "x", "<F3>", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
-                keymap(bufnr, "n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-                keymap(bufnr, "n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-
-                -- Trigger signature help when typing a function delcaration
-                if client.server_capabilities.signatureHelpProvider then
-                    vim.api.nvim_create_autocmd("CursorHoldI", {
-                        buffer = bufnr,
-                        callback = function()
-                            vim.lsp.buf.signature_help()
-                        end,
+            -- Shared capabilities & on_attach
+            local capabilities = cmp_nvim_lsp.default_capabilities(
+                vim.lsp.protocol.make_client_capabilities()
+            )
+            local on_attach    = function(client, bufnr)
+                local bufmap = function(mode, lhs, rhs)
+                    vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, {
+                        noremap = true,
+                        silent  = true,
                     })
                 end
-            end)
 
-            lsp.setup()
+                -- LSP keybindings
+                bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+                bufmap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+                bufmap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+                bufmap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+                bufmap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+                bufmap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+                bufmap("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<CR>")
+                bufmap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+                bufmap("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+                bufmap("n", "<F3>", "<cmd>lua vim.lsp.buf.format()<CR>")
+                bufmap("x", "<F3>", "<cmd>lua vim.lsp.buf.format()<CR>")
 
-            -- Autocompletion settings
-            local cmp = require("cmp")
-            cmp.setup({
-                mapping = {
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                    ["<C-Space>"] = cmp.mapping.complete(),
+                -- signature help on CursorHoldI if supported
+                if client.server_capabilities.signatureHelpProvider then
+                    vim.api.nvim_create_autocmd("CursorHoldI", {
+                        buffer   = bufnr,
+                        callback = vim.lsp.buf.signature_help,
+                    })
+                end
+            end
+
+            -- Single setup, with `handlers` table (v2 API)
+            mlsp.setup {
+                ensure_installed = {
+                    "lua_ls",        -- Lua
+                    "pyright",       -- Python
+                    "ts_ls",         -- TS/JS
+                    "clangd",        -- C/C++
+                    "marksman",      -- Markdown
+                    "rust_analyzer", -- Rust
                 },
+                automatic_installation = true,
+                handlers = {
+                    -- default handler for all servers
+                    function(server_name)
+                        lspconfig[server_name].setup {
+                            on_attach    = on_attach,
+                            capabilities = capabilities,
+                        }
+                    end,
+
+                    -- override lua_ls to recognize `vim` global
+                    ["lua_ls"] = function()
+                        lspconfig.lua_ls.setup {
+                            on_attach    = on_attach,
+                            capabilities = capabilities,
+                            settings     = {
+                                Lua = {
+                                    diagnostics = {
+                                        globals = { "vim" },
+                                    },
+                                },
+                            },
+                        }
+                    end,
+
+                    -- override marksman to disable signatureHelp
+                    ["marksman"] = function()
+                        lspconfig.marksman.setup {
+                            on_attach    = on_attach,
+                            capabilities = capabilities,
+                            handlers     = {
+                                ["textDocument/signatureHelp"] = function() end,
+                            },
+                        }
+                    end,
+                },
+            }
+
+            -- Format on save
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                pattern = "*.lua,*.rs,*.ts,*.py",
+                callback = function()
+                    vim.lsp.buf.format({ async = false })
+                end,
+            })
+        end,
+    },
+
+    ------------------------------------------------------------------------------
+    -- Completion: nvim-cmp + snippet support
+    ------------------------------------------------------------------------------
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-nvim-lsp-signature-help",
+            "saadparwaiz1/cmp_luasnip",
+            "L3MON4D3/LuaSnip",
+        },
+        config = function()
+            local cmp     = require("cmp")
+            local luasnip = require("luasnip")
+
+            cmp.setup {
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<CR>"]      = cmp.mapping.confirm({ select = true }),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                }),
                 sources = {
                     { name = "nvim_lsp" },
+                    { name = "nvim_lsp_signature_help" },
                     { name = "buffer" },
                     { name = "path" },
-                    { name = "nvim_lsp_signature_help" }
+                    { name = "luasnip" },
                 },
-            })
+            }
         end,
     },
 }
