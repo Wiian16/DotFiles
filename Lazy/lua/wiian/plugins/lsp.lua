9eb5cb9b8faf257e8a2fcf803e55154a16bd0f0c
Ian McGillivary <wiian16@icloud.com>
2025-08-25 21:25:58 -0400

    Change lsp on_attach to LspAttach autocommand

--- Lazy/lua/wiian/plugins/lsp.lua
+++ Lazy/lua/wiian/plugins/lsp.lua
@@ -11,19 +11,19 @@ return {
             -- Enable all diagnostic displays
             vim.diagnostic.config({
                 virtual_text = {
-                    prefix = "■", -- Could be '●', '■', '▎', etc.
-                    spacing = 2, -- How much space between text and virtual text
+                    prefix = "■",
+                    spacing = 2,
                 },
-                signs = true, -- Show icons in the sign column
-                underline = true, -- Underline problematic text
-                update_in_insert = false, -- Don’t update diagnostics while in insert mode
-                severity_sort = true, -- Sort diagnostics by severity
+                signs = true,
+                underline = true,
+                update_in_insert = false,
+                severity_sort = true,
             })
         end,
     },
 
     ------------------------------------------------------------------------------
-    -- Mason-LSPConfig v2: bridge Mason & nvim-lspconfig, now with `handlers` in setup
+    -- Mason-LSPConfig: bridge Mason & nvim-lspconfig
     ------------------------------------------------------------------------------
     {
         "mason-org/mason-lspconfig.nvim",
@@ -36,88 +36,49 @@ return {
             local lspconfig    = require("lspconfig")
             local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-            -- Shared capabilities & on_attach
             local capabilities = cmp_nvim_lsp.default_capabilities(
                 vim.lsp.protocol.make_client_capabilities()
             )
-            local on_attach    = function(client, bufnr)
-                local bufmap = function(mode, lhs, rhs)
-                    vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, {
-                        noremap = true,
-                        silent  = true,
-                    })
-                end
 
-                -- LSP keybindings
-                bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
-                bufmap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
-                bufmap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
-                bufmap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-                bufmap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
-                bufmap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
-                bufmap("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<CR>")
-                bufmap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-                bufmap("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-                bufmap("n", "<F3>", "<cmd>lua vim.lsp.buf.format()<CR>")
-                bufmap("x", "<F3>", "<cmd>lua vim.lsp.buf.format()<CR>")
-
-                -- signature help on CursorHoldI if supported
-                if client.server_capabilities.signatureHelpProvider then
-                    vim.api.nvim_create_autocmd("CursorHoldI", {
-                        buffer   = bufnr,
-                        callback = vim.lsp.buf.signature_help,
-                    })
-                end
-            end
-
-            -- Single setup, with `handlers` table (v2 API)
             mlsp.setup {
                 ensure_installed = {
-                    "lua_ls",        -- Lua
-                    "pyright",       -- Python
-                    "ts_ls",         -- TS/JS
-                    "clangd",        -- C/C++
-                    "marksman",      -- Markdown
-                    "rust_analyzer", -- Rust
+                    "lua_ls", "pyright", "ts_ls", "clangd",
+                    "marksman", "rust_analyzer",
                 },
                 automatic_installation = true,
                 handlers = {
-                    -- default handler for all servers
+                    -- default handler
                     function(server_name)
                         lspconfig[server_name].setup {
-                            on_attach    = on_attach,
                             capabilities = capabilities,
                         }
                     end,
 
-                    -- override lua_ls to recognize `vim` global
+                    -- lua_ls: add vim as global
                     ["lua_ls"] = function()
                         lspconfig.lua_ls.setup {
-                            on_attach    = on_attach,
                             capabilities = capabilities,
-                            settings     = {
+                            settings = {
                                 Lua = {
-                                    diagnostics = {
-                                        globals = { "vim" },
-                                    },
+                                    diagnostics = { globals = { "vim" } },
                                 },
                             },
                         }
                     end,
 
-                    -- override marksman to disable signatureHelp
+                    -- marksman: disable signatureHelp (annoying)
                     ["marksman"] = function()
                         lspconfig.marksman.setup {
-                            on_attach    = on_attach,
                             capabilities = capabilities,
-                            handlers     = {
+                            handlers = {
                                 ["textDocument/signatureHelp"] = function() end,
                             },
                         }
                     end,
 
+                    -- omnisharp: extra flags
                     ["omnisharp"] = function()
-                        lspconfig.omnisharp.setup({
+                        lspconfig.omnisharp.setup {
                             cmd = { "omnisharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
                             enable_editorconfig_support = true,
                             enable_roslyn_analyzers = true,
@@ -125,12 +86,50 @@ return {
                             enable_import_completion = true,
                             sdk_include_prereleases = true,
                             analyze_open_documents_only = false,
-                        })
+                            capabilities = capabilities,
+                        }
                     end,
                 },
             }
 
+            ------------------------------------------------------------------------------
+            -- Global LSP keymaps (applied via LspAttach, not per-server on_attach)
+            ------------------------------------------------------------------------------
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+                callback = function(ev)
+                    local bufnr = ev.buf
+                    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+
+                    local bufmap = function(mode, lhs, rhs)
+                        vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, silent = true, noremap = true })
+                    end
+
+                    -- LSP keymaps
+                    bufmap("n", "gd", vim.lsp.buf.definition)
+                    bufmap("n", "gD", vim.lsp.buf.declaration)
+                    bufmap("n", "gi", vim.lsp.buf.implementation)
+                    bufmap("n", "gr", vim.lsp.buf.references)
+                    bufmap("n", "K", vim.lsp.buf.hover)
+                    bufmap("n", "<leader>rn", vim.lsp.buf.rename)
+                    bufmap("n", "<F2>", vim.lsp.buf.rename)
+                    bufmap("n", "<leader>ca", vim.lsp.buf.code_action)
+                    bufmap("n", "<F4>", vim.lsp.buf.code_action)
+                    bufmap({ "n", "x" }, "<F3>", vim.lsp.buf.format)
+
+                    -- optional: signature help
+                    if client and client.server_capabilities.signatureHelpProvider then
+                        vim.api.nvim_create_autocmd("CursorHoldI", {
+                            buffer = bufnr,
+                            callback = vim.lsp.buf.signature_help,
+                        })
+                    end
+                end,
+            })
+
+            ------------------------------------------------------------------------------
             -- Format on save
+            ------------------------------------------------------------------------------
             vim.api.nvim_create_autocmd("BufWritePre", {
                 callback = function()
                     vim.lsp.buf.format({ async = false })
