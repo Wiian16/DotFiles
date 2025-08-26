5339c15c613e0f1786b410a5f1bcab2ae762864b
Ian McGillivary <wiian16@icloud.com>
2025-08-25 20:41:21 -0400

    Add omnisharp lsp config, remove restriction on format on save

--- Lazy/lua/wiian/plugins/lsp.lua
+++ Lazy/lua/wiian/plugins/lsp.lua
@@ -115,12 +115,23 @@ return {
                             },
                         }
                     end,
+
+                    ["omnisharp"] = function()
+                        lspconfig.omnisharp.setup({
+                            cmd = { "omnisharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
+                            enable_editorconfig_support = true,
+                            enable_roslyn_analyzers = true,
+                            organize_imports_on_format = true,
+                            enable_import_completion = true,
+                            sdk_include_prereleases = true,
+                            analyze_open_documents_only = false,
+                        })
+                    end,
                 },
             }
 
             -- Format on save
             vim.api.nvim_create_autocmd("BufWritePre", {
-                pattern = "*.lua,*.rs,*.ts,*.py",
                 callback = function()
                     vim.lsp.buf.format({ async = false })
                 end,
